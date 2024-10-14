41ff94cfeca827cf57c8a0e7356a12b47b0dbec6
Cheng-Yu Chung <chengyu.chung@gosaico.com>
2024-10-14 10:37:01 +0800

    Added ui state

--- app/src/main/java/com/example/weatherapp/ui/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ui/WeatherViewModel.kt
@@ -2,7 +2,6 @@ package com.example.weatherapp.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.weatherapp.network.ApiService
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
